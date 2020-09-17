54a7a4b6d631282fe244dc15e51e3d14b3a09d00
Jesse Zarat <jczarat@gmail.com>
2020-09-17 16:10:34 -0600

    finish part 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.urls import reverse
 
+# the first item represents the value that will be stored in the database.
+# The second item represents the human-friendly "display" value.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 
 class Dog(models.Model):
@@ -14,3 +23,15 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']

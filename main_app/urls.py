a4eab542e7cb212d2cbe266b3e5335768fb2c461
Jesse Zarat <jczarat@gmail.com>
2020-09-17 19:07:14 -0600

    finish part 5

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
     path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+
+    path('dogs/<int:dog_id>/add_toy/<int:toy_id>', views.add_toy, name='add_toy'),
+
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import FeedingForm
 
 # Create your views here.
-
-
-def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def dogs_index(request):
-    dogs = Dog.objects.all()
-    return render(request, 'dogs/index.html', {'dogs': dogs})
-
-
-def dogs_detail(request, dog_id):
-    dog = Dog.objects.get(id=dog_id)
-    feeding_form = FeedingForm()
-    return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form})
 
 
 class DogCreate(CreateView):
@@ -42,6 +22,31 @@ class DogDelete(DeleteView):
     model = Dog
     success_url = '/dogs/'
 
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'dogs/detail.html', {
+        'dog': dog, 
+        'feeding_form': feeding_form, 
+        'toys': toys_dog_doesnt_have
+        })
+
+
 def add_feeding(request, dog_id):
     form = FeedingForm(request.POST)
     if (form.is_valid):
@@ -49,3 +54,30 @@ def add_feeding(request, dog_id):
         new_feeding.dog_id = dog_id
         new_feeding.save()
     return redirect('detail', dog_id=dog_id)
+
+def add_toy(request, dog_id, toy_id):
+    dog = Dog.objects.get(id=dog_id).toys.add(toy_id)
+    return redirect('detail', dog_id=dog_id)
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
